88d0332f94aa9ee889d2526e1ecfd36510b15fdc
Wylker Porto <dragonester@hotmail.com>
2017-10-03 14:39:53 -0300

    prototipo_1

---
+++ Codigos/teste.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teste : MonoBehaviour {
+	public bool states;
+
+	// Use this for initialization
+	void Start () {
+		states = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision colisor){
+		if (states) {
+			printa ();
+			states = !states;
+		}
+	}
+
+	void printa (){
+		print ("Tocou");
+	}
+}
---
+++ Codigos/teste2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teste2 : MonoBehaviour {
+	public bool states;
+
+	// Use this for initialization
+	void Start () {
+		states = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision colisor){
+		if (states) {
+			print ("Tocou2");
+			states = !states;
+		}
+	}
+}
